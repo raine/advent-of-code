15fac4b7b8d0e50657d6bb7e9a97b70b4b20b418
Raine Virta <raine.virta@gmail.com>
2022-12-13 00:09:52 +0200

    day 6 part 2

--- 2022/src/day6.rs
+++ 2022/src/day6.rs
@@ -1,15 +1,15 @@
 use itertools::Itertools;
 
-fn day6(input: &str) -> Option<usize> {
+fn day6(input: &str, distinct_count: usize) -> Option<usize> {
     for (idx, slice) in input
         .chars()
         .collect::<Vec<char>>()
         .as_slice()
-        .windows(4)
+        .windows(distinct_count)
         .enumerate()
     {
         if slice.iter().all_unique() {
-            return Some(idx + 4);
+            return Some(idx + distinct_count);
         }
     }
 
@@ -23,6 +23,12 @@ mod tests {
     #[test]
     fn day6_test() {
         let input = include_str!("../testdata/day6");
-        assert_eq!(day6(input), Some(1723));
+        assert_eq!(day6(input, 4), Some(1723));
+    }
+
+    #[test]
+    fn day6_part2_test() {
+        let input = include_str!("../testdata/day6");
+        assert_eq!(day6(input, 14), Some(3708));
     }
 }

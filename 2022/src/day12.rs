021a6c6bbfaae8fa4f11a6bf2be6f29ce09d412d
Raine Virta <raine.virta@gmail.com>
2022-12-25 22:30:56 +0200

    day 12

---
+++ 2022/src/day12.rs
@@ -0,0 +1,195 @@
+use std::char;
+use std::collections::{HashMap, HashSet, VecDeque};
+use std::str::FromStr;
+
+#[derive(Debug)]
+struct Heightmap {
+    cells: Vec<Cell>,
+    height: usize,
+    width: usize,
+}
+
+impl Heightmap {
+    fn get_cell_idx(&self, x: i64, y: i64) -> Option<usize> {
+        match (x, y) {
+            (x, y) if x >= 0 && y >= 0 => Some((x + (self.width as i64 * y)) as usize),
+            (_, _) => None,
+        }
+    }
+
+    fn get_cell(&self, x: i64, y: i64) -> Option<(&Cell, usize)> {
+        self.get_cell_idx(x, y)
+            .and_then(|idx| self.cells.get(idx).map(|c| (c, idx)))
+    }
+
+    fn get_adjacent_cell_idxs(&self, x: i64, y: i64) -> Vec<usize> {
+        let (cell, _idx) = self.get_cell(x, y).unwrap();
+        let left = self.get_cell(x - 1, y);
+        let right = self.get_cell(x + 1, y);
+        let top = self.get_cell(x, y - 1);
+        let bottom = self.get_cell(x, y + 1);
+        let sides = [left, right, top, bottom];
+
+        sides
+            .iter()
+            .filter_map(|x| match *x {
+                Some((c, idx)) if c.elevation() <= cell.elevation() + 1 => Some(idx),
+                Some(_) => None,
+                None => None,
+            })
+            .collect::<Vec<usize>>()
+    }
+
+    fn get_start_idx(&self) -> usize {
+        let (idx, _cell) = self
+            .cells
+            .iter()
+            .enumerate()
+            .find(|(_idx, cell)| matches!(cell, Cell::Start))
+            .unwrap();
+
+        idx
+    }
+
+    fn get_end_idx(&self) -> usize {
+        let (idx, _cell) = self
+            .cells
+            .iter()
+            .enumerate()
+            .find(|(_idx, cell)| matches!(cell, Cell::End))
+            .unwrap();
+
+        idx
+    }
+
+    fn to_adjacency_list(&self) -> HashMap<usize, Vec<usize>> {
+        let mut list = HashMap::new();
+
+        for y in 0..self.height as i64 {
+            for x in 0..self.width as i64 {
+                let (_cell, idx) = self.get_cell(x, y).unwrap();
+                list.insert(idx, self.get_adjacent_cell_idxs(x, y));
+            }
+        }
+
+        list
+    }
+}
+
+impl FromStr for Heightmap {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let lines = s.lines();
+        let height = lines.clone().count();
+        let width = lines.clone().next().unwrap().len();
+        let cells = lines
+            .flat_map(|l| l.chars().map(parse_cell).collect::<Vec<Cell>>())
+            .collect();
+
+        Ok(Heightmap {
+            cells,
+            height,
+            width,
+        })
+    }
+}
+
+#[derive(Debug, Clone)]
+enum Cell {
+    Start,
+    End,
+    Square(u8),
+}
+
+const MIN_ELEVATION: u8 = 0;
+const MAX_ELEVATION: u8 = b'z' - 97;
+
+impl Cell {
+    fn elevation(&self) -> u8 {
+        match self {
+            Cell::Start => MIN_ELEVATION,
+            Cell::End => MAX_ELEVATION,
+            Cell::Square(e) => *e,
+        }
+    }
+}
+
+fn parse_cell(c: char) -> Cell {
+    match c {
+        'S' => Cell::Start,
+        'E' => Cell::End,
+        c => Cell::Square((c as u8) - b'a'),
+    }
+}
+
+fn bfs(graph: HashMap<usize, Vec<usize>>, start: usize, end: usize) -> Option<Vec<usize>> {
+    let mut queue = VecDeque::new();
+    queue.push_back(start);
+
+    let mut visited = HashSet::new();
+    visited.insert(start);
+
+    let mut prev_map: HashMap<usize, usize> = HashMap::new();
+    'outer: while !queue.is_empty() {
+        let current_node = queue.pop_front().unwrap();
+        for v in graph.get(&current_node).unwrap().iter() {
+            if *v == end {
+                prev_map.insert(*v, current_node);
+                break 'outer;
+            }
+
+            if !visited.contains(v) {
+                prev_map.insert(*v, current_node);
+                queue.push_back(*v);
+                visited.insert(*v);
+            }
+        }
+    }
+
+    let mut path = Vec::new();
+    let mut prev = Some(end);
+    while let Some(p) = prev {
+        path.push(p);
+        prev = prev_map.get(&p).cloned();
+    }
+    path.reverse();
+
+    match path.first() {
+        Some(node) if *node == start => Some(path),
+        _ => None,
+    }
+}
+
+fn day12(input: &str) -> usize {
+    let map: Heightmap = input.trim_start_matches('\n').parse().unwrap();
+    let adjacency_list = map.to_adjacency_list();
+    let start = map.get_start_idx();
+    let end = map.get_end_idx();
+    let path = bfs(adjacency_list, start, end);
+    path.unwrap().len() - 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn day12_simple_test() {
+        let input = "
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
+";
+
+        assert_eq!(day12(input), 31);
+    }
+
+    #[test]
+    fn day12_test() {
+        let input = include_str!("../testdata/day12");
+        assert_eq!(day12(input), 361);
+    }
+}
